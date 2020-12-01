320ba54274d712774619fa8fde94b05b782fc622
vigneshwari2811 <54399735+vigneshwari2811@users.noreply.github.com>
2020-12-01 21:00:13 +0530

    Add files via upload

---
+++ pot.py
@@ -0,0 +1,87 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+import cv2
+import tensorflow as tf
+from tensorflow import keras
+# dimensions of our images.
+img_width, img_height = 280, 150
+
+train_data_dir = '/content/drive/MyDrive/road1/training'
+validation_data_dir = '/content/drive/MyDrive/road1/testing'
+nb_train_samples = 224
+nb_validation_samples = 224
+epochs = 50
+batch_size = 16
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size)
+model.save_weights('first_try.h5')
+
+CATEGORIES=['normal','potholes']
+def prepare(fliepath):
+  IMG_SIZE=70
+  img_array=cv2.imread(filepath,c2.IMREAD_GREYSCALE)
+  new_array=cv2.resize(img_array,(IMG_SIZE,IMG_SIZE))
+  return new_array.reshape(-1,IMG_SIZE,IMG_SIZE,1)
+model=tf.keras.models.load_model('first_try.h5')
+prediction=model.predict([prepare('/content/drive/MyDrive/potholes.jpg')])
+print(CATEGORIES[int(prediction[0][0])])
